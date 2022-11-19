0adfbed0fce40370a910229d0b9952bf7fbe8a2f
RosasIrma <irmaceless@gmail.com>
2022-11-19 14:35:53 -0300

    cambios

---
+++ js/carrousel.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    const carrouselImage1 = document.getElementById("carrousel-image-1");
+    carrouselImage1.style.backgroundImage = 'url("galli.jpg")';
+    carrouselImage1.style.backgroundImage = 'url("pingui.jpg")';
+    carrouselImage1.style.backgroundImage = 'url("cerdi.jpg")';
+}
